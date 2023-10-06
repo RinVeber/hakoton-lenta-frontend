546344dab62840a8c0efddff9131c4b0c7eeffa8
Kirill Karpov <rin.veber@gmail.com>
2023-10-06 14:31:58 +0400

    fix sort icon

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -2,43 +2,56 @@ import React from "react";
 import styles from "./Table.module.css";
 import up from "../../assets/filter-up.svg";
 import down from "../../assets/filter-down.svg";
-import {mokDataSource} from '../../utils/constant';
+import downActive from "../../assets/filter-down-active.svg";
+import upActive from "../../assets/filter-up-active.svg";
+import { mokDataSource } from "../../utils/constant";
 import { columnsTable } from "../../types/types";
 
 interface TableProps {
-  mokColumns: columnsTable[]
+  mokColumns: columnsTable[];
 }
 
-export default function Table({mokColumns} : TableProps) {
-
+export default function Table({ mokColumns }: TableProps) {
   const [sortList, setSortList] = React.useState(mokDataSource);
+  const [lastSortTitle, setlastSortTitle] = React.useState("");
+
   const tableRef = React.useRef<HTMLDivElement>(null);
 
-  const customSort = (sortField : string) => {
+  const customSort = (sortField: string, type: string) => {
     return function (a: any, b: any) {
-       if (a[sortField] < b[sortField]) {
-         return -1;
-       }
-       if (a[sortField] > b[sortField]) {
-         return 1;
-       }
-       return 0;
-     }
-   }
-   const handlerSort = (sortType: string) => {
-    console.log(sortType)
-     if (sortType === "TK") {
-       setSortList([...sortList.sort(customSort("name"))]);
-     } else if (sortType === "Группа") {
-       setSortList([...sortList.sort(customSort("group"))]);
-     } else if (sortType === "Категория") {
-       setSortList([...sortList.sort(customSort("category"))]);
-     } else if (sortType === "Подкатегория") {
-       setSortList([...sortList.sort(customSort("podcategory"))]);
-     } else if (sortType === "SKU") {
-       setSortList([...sortList.sort(customSort("sku"))]);
-     }
-   };
+      if (type == "add") {
+        if (a[sortField] < b[sortField]) {
+          return -1;
+        }
+        if (a[sortField] > b[sortField]) {
+          return 1;
+        }
+        return 0;
+      } else {
+        if (a[sortField] > b[sortField]) {
+          return -1;
+        }
+        if (a[sortField] < b[sortField]) {
+          return 1;
+        }
+        return 0;
+      }
+    };
+  };
+  const handlerSort = (sortType: string, type: string) => {
+    console.log(sortType);
+    if (sortType === "TK") {
+      setSortList([...sortList.sort(customSort("name", type))]);
+    } else if (sortType === "Группа") {
+      setSortList([...sortList.sort(customSort("group", type))]);
+    } else if (sortType === "Категория") {
+      setSortList([...sortList.sort(customSort("category", type))]);
+    } else if (sortType === "Подкатегория") {
+      setSortList([...sortList.sort(customSort("podcategory", type))]);
+    } else if (sortType === "SKU") {
+      setSortList([...sortList.sort(customSort("sku", type))]);
+    }
+  };
 
   return (
     <section className={styles.table} ref={tableRef}>
@@ -49,15 +62,24 @@ export default function Table({mokColumns} : TableProps) {
               <div className={styles.table__columnName}>{item.title}</div>
               <div className={styles.table__columnFilter}>
                 <img
-                  src={up}
+                  src={lastSortTitle == item.title + "up" ? upActive : up }
                   className={styles.table__columnFilter_icon}
                   alt="иконка"
-                  onClick={() => (handlerSort(item.title))}
+                  onClick={() => {
+                    const typeAdd = "add";
+                    setlastSortTitle(item.title + "up");
+                    handlerSort(item.title, typeAdd);
+                  }}
                 />
                 <img
-                  src={down}
+                  src={lastSortTitle == item.title + "down" ? downActive : down}
                   className={styles.table__columnFilter_icon}
                   alt="иконка"
+                  onClick={() => {
+                    const typeDecrase = "decrease";
+                    setlastSortTitle(item.title + "down");
+                    handlerSort(item.title, typeDecrase);
+                  }}
                 />
               </div>
             </div>
