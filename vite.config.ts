9b13740c676ffa9458c9c423dd5eda554f3ac71a
Кирилл Карпов <96541093+RinVeber@users.noreply.github.com>
2023-10-10 01:09:56 +0400

    Test (#11)
    * auth test
    
    * initial commit - layout
    
    * fix sort icon
    
    * fix margin last row
    
    * fix store forcast
    
    * check
    
    * auth
    
    * dev table mui version
    
    * чистка кода
    
    * delete table mui
    
    * merchge branch auth + fix type in formSlice
    
    * fix table
    
    * fix sorted function
    
    * чистка кода
    
    * fixed header table
    
    * form searchData in modal
    
    * fix select list and chips
    
    * add blur fix
    
    * test date
    
    * fix link page
    
    * dev table forecast
    
    * fix sort function
    
    * fix state formSearch
    
    * dev table salesDiff
    
    * fix bottom scroll in tableSales
    
    * fix sort function in tableSales
    
    * refactor structure table in project
    
    * fix import
    
    * fix scroll
    
    * add click open Modal second page
    
    * фикс ошибки перезаписи изначально загруженных данных и результата поиска
    
    * fix api
    
    * fix newForecast
    
    * fixed
    
    * fixed layout
    
    * fixed button forcast
    
    * fix header width
    
    * fiex layot
    
    * fix layout
    
    * fix loader
    
    * fix color link
    
    * fix martin tabs
    
    * fix scroll
    
    * fix bug duble state in scroll
    
    * fix readme
    
    * fix
    
    * fix notSKUfound
    
    * fixed notSKUfound
    
    * fix for gh pages
    
    * fix pacjage json
    
    * fix putthname
    
    ---------
    
    Co-authored-by: Artem2088 <jablonsky.art@yndex.ru>

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hakoton-lenta-frontend',
+
+  // base: '/hakoton-lenta-frontend',
+
   plugins: [react()],
 })
